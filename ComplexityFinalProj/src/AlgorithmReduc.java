1ac672a674c7692ea6738409f7b6e7b333f4356c
Jack Steketee <jstekete@skidmore.edu>
2017-04-04 19:30:49 -0400

    Algorithms Project

---
+++ ComplexityFinalProj/src/AlgorithmReduc.java
@@ -0,0 +1,16 @@
+
+/**
+ * Algorithm to solve 0-1 Knapsack using a reduction from 3
+ * 
+ * @author jstekete
+ *
+ */
+public class AlgorithmReduc {
+
+	static final double EPSILON = 0.1;
+
+	public static int solve(Itemset itemset, int capacity) {
+	
+		return 0;
+	}
+}
---
+++ ComplexityFinalProj/src/Item.java
@@ -0,0 +1,11 @@
+
+public class Item {
+
+	int value;
+	int weight;
+
+	public Item(int v, int w) {
+		this.value = v;
+		this.weight = w;
+	}
+}
